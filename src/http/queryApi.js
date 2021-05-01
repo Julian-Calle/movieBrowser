efe4ac9241ad1c8d3f9bc6a1a739d3726385ea41
Julian-Calle <jcallecristancho@gmail.com>
2021-05-02 00:14:43 +0200

    env var

--- src/http/queryApi.js
+++ src/http/queryApi.js
@@ -1,14 +1,13 @@
-const { REACT_APP_APIKEY } = process.env;
+// const { REACT_APP_APIKEY } = process.env;
+const { APIKEY } = process.env;
 
 export async function searchMovie(title, page) {
-  const resultQuery = await (
-    await fetch(`http://www.omdbapi.com/?s=${title}&apikey=${REACT_APP_APIKEY}&page=${page}`)
-  ).json();
+  const resultQuery = await (await fetch(`http://www.omdbapi.com/?s=${title}&apikey=${APIKEY}&page=${page}`)).json();
   return resultQuery.Search;
 }
 
 export async function detailsMovie(idDeIMDB) {
-  const resultQuery = await (await fetch(`http://www.omdbapi.com/?i=${idDeIMDB}&apikey=${REACT_APP_APIKEY}`)).json();
+  const resultQuery = await (await fetch(`http://www.omdbapi.com/?i=${idDeIMDB}&apikey=${APP_APIKEY}`)).json();
 
   return resultQuery;
 }
